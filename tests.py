5eac795b2cf60099b896074e2304e6fc71d09abe
Vladimirsky <vladimirlatch@yandex.ru>
2025-06-23 11:03:50 +0300

    Готовый проект из 4 спринта на ревью

--- tests.py
+++ tests.py
@@ -18,7 +18,94 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book_does_not_add_duplicate(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.add_new_book('Дюна')
+        assert len(collector.get_books_genre()) == 1
+
+    def test_add_new_book_does_not_add_if_name_too_long(self):
+        collector = BooksCollector()
+        long_name = 'А' * 41
+        collector.add_new_book(long_name)
+        assert len(collector.get_books_genre()) == 0
+
+    def test_set_book_genre_sets_valid_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.set_book_genre('Дюна', 'Фантастика')
+        assert collector.get_book_genre('Дюна') == 'Фантастика'
+
+    def test_set_book_genre_does_not_set_invalid_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.set_book_genre('Дюна', 'Романтика')
+        assert collector.get_book_genre('Дюна') == ''
+
+    def test_get_book_genre_returns_correct_value(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.set_book_genre('Дюна', 'Фантастика')
+        assert collector.get_book_genre('Дюна') == 'Фантастика'
+
+    def test_get_books_with_specific_genre_returns_only_matching(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Дюна', 'Фантастика')
+        collector.set_book_genre('Оно', 'Ужасы')
+        result = collector.get_books_with_specific_genre('Фантастика')
+        assert result == ['Дюна']
+
+    def test_get_books_genre_returns_books_dict(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        result = collector.get_books_genre()
+        assert result == {'Дюна': ''}
+
+    def test_get_books_for_children_returns_only_safe_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Оно', 'Ужасы')
+        collector.add_new_book('Тачки')
+        collector.set_book_genre('Тачки', 'Мультфильмы')
+        result = collector.get_books_for_children()
+        assert result == ['Тачки']
+
+    def test_add_book_in_favorites_adds_once(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.add_book_in_favorites('Дюна')
+        assert collector.get_list_of_favorites_books() == ['Дюна']
+
+    def test_add_book_in_favorites_does_not_add_twice(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.add_book_in_favorites('Дюна')
+        collector.add_book_in_favorites('Дюна')
+        assert collector.get_list_of_favorites_books().count('Дюна') == 1
+
+    def test_add_book_in_favorites_does_not_add_if_book_unknown(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Неизвестная')
+        assert 'Неизвестная' not in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites_removes_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.add_book_in_favorites('Дюна')
+        collector.delete_book_from_favorites('Дюна')
+        assert collector.get_list_of_favorites_books() == []
+
+    def test_get_list_of_favorites_books_returns_correct_list(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.add_new_book('Оно')
+        collector.add_book_in_favorites('Дюна')
+        collector.add_book_in_favorites('Оно')
+        assert collector.get_list_of_favorites_books() == ['Дюна', 'Оно']
